eba1a80e149f2574f5a3239aa8f102ecf3c7b928
Moritz Biering <moritzbiering.mb@gmail.com>
2025-11-16 12:08:39 +0100

    Refactor logger implementation to use slog package and enhance logging options

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -2,7 +2,8 @@ package logger
 
 import (
 	"fmt"
-	"log"
+	"io"
+	"log/slog"
 	"os"
 )
 
@@ -15,35 +16,123 @@ type Logger interface {
 }
 
 var logger Logger
+var currentOpts LoggerOptions
 
-type defaultLogger struct {
-	logger *log.Logger
+type slogLogger struct {
+	logger *slog.Logger
 }
 
-func newDefaultLogger() *defaultLogger {
-	return &defaultLogger{
-		logger: log.New(os.Stdout, "go-let-loose: ", log.LstdFlags),
+// LogLevel represents the minimum log level
+type LogLevel int
+
+const (
+	LevelDebug LogLevel = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+)
+
+// String returns the string representation of the log level
+func (l LogLevel) String() string {
+	switch l {
+	case LevelDebug:
+		return "debug"
+	case LevelInfo:
+		return "info"
+	case LevelWarn:
+		return "warn"
+	case LevelError:
+		return "error"
+	default:
+		return "info"
 	}
 }
 
-func (l *defaultLogger) Debug(msg string) {
-	l.logger.Print("[DEBUG] " + msg)
+// ParseLogLevel converts a string to a LogLevel
+func ParseLogLevel(s string) LogLevel {
+	switch s {
+	case "debug":
+		return LevelDebug
+	case "info":
+		return LevelInfo
+	case "warn":
+		return LevelWarn
+	case "error":
+		return LevelError
+	default:
+		return LevelInfo
+	}
 }
 
-func (l *defaultLogger) Info(msg string) {
-	l.logger.Print("[INFO] " + msg)
+// LoggerOptions configures the logger behavior
+type LoggerOptions struct {
+	Level      LogLevel
+	AddSource  bool // Add source file/line information
+	JSONFormat bool // Use JSON format instead of text
+	Writer     io.Writer
 }
 
-func (l *defaultLogger) Warn(msg string) {
-	l.logger.Print("[WARN] " + msg)
+// DefaultOptions returns sensible default options
+func DefaultOptions() LoggerOptions {
+	return LoggerOptions{
+		Level:      LevelInfo,
+		AddSource:  true,
+		JSONFormat: false,
+		Writer:     os.Stdout,
+	}
 }
 
-func (l *defaultLogger) Error(msg string) {
-	l.logger.Print("[ERROR] " + msg)
+func newSlogLogger(opts LoggerOptions) *slogLogger {
+	var level slog.Level
+	switch opts.Level {
+	case LevelDebug:
+		level = slog.LevelDebug
+	case LevelInfo:
+		level = slog.LevelInfo
+	case LevelWarn:
+		level = slog.LevelWarn
+	case LevelError:
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	handlerOpts := &slog.HandlerOptions{
+		Level:     level,
+		AddSource: opts.AddSource,
+	}
+
+	var handler slog.Handler
+	if opts.JSONFormat {
+		handler = slog.NewJSONHandler(opts.Writer, handlerOpts)
+	} else {
+		handler = slog.NewTextHandler(opts.Writer, handlerOpts)
+	}
+
+	return &slogLogger{
+		logger: slog.New(handler),
+	}
 }
 
-func (l *defaultLogger) Fatal(msg string) {
-	l.logger.Print("[FATAL] " + msg)
+func (l *slogLogger) Debug(msg string) {
+	l.logger.Debug(msg)
+}
+
+func (l *slogLogger) Info(msg string) {
+	l.logger.Info(msg)
+}
+
+func (l *slogLogger) Warn(msg string) {
+	l.logger.Warn(msg)
+}
+
+func (l *slogLogger) Error(msg string) {
+	l.logger.Error(msg)
+}
+
+func (l *slogLogger) Fatal(msg string) {
+	l.logger.Error(msg)
+	os.Exit(1)
 }
 
 type nopLogger struct{}
@@ -52,58 +141,68 @@ func newNOPLogger() *nopLogger {
 	return &nopLogger{}
 }
 
-func (l *nopLogger) Debug(msg string) {
-}
+func (l *nopLogger) Debug(msg string) {}
 
-func (l *nopLogger) Info(msg string) {
-}
+func (l *nopLogger) Info(msg string) {}
 
-func (l *nopLogger) Warn(msg string) {
-}
+func (l *nopLogger) Warn(msg string) {}
 
-func (l *nopLogger) Error(msg string) {
-}
+func (l *nopLogger) Error(msg string) {}
 
-func (l *nopLogger) Fatal(msg string) {
-}
+func (l *nopLogger) Fatal(msg string) {}
 
+// DefaultLogger initializes the logger with default settings (Info level, text format)
 func DefaultLogger() {
-	logger = newDefaultLogger()
+	currentOpts = DefaultOptions()
+	logger = newSlogLogger(currentOpts)
 }
 
+// NOPLogger initializes a no-operation logger that discards all logs
 func NOPLogger() {
 	logger = newNOPLogger()
+}
+
+// InitLogger initializes the logger with custom options
+func InitLogger(opts LoggerOptions) {
+	currentOpts = opts
+	logger = newSlogLogger(opts)
+}
+
+// SetLogLevel changes the log level of the current logger
+func SetLogLevel(level LogLevel) {
+	currentOpts.Level = level
+	logger = newSlogLogger(currentOpts)
 }
 
 func SetLogger(l Logger) {
 	logger = l
 }
 
-func Debug(v ...interface{}) {
+func Debug(v ...any) {
 	if logger != nil {
 		logger.Debug(fmt.Sprintln(v...))
 	}
 }
 
-func Info(v ...interface{}) {
+func Info(v ...any) {
 	if logger != nil {
 		logger.Info(fmt.Sprintln(v...))
 	}
 }
 
-func Warn(v ...interface{}) {
+func Warn(v ...any) {
 	if logger != nil {
 		logger.Warn(fmt.Sprintln(v...))
 	}
 }
 
-func Error(v ...interface{}) {
+func Error(v ...any) {
 	if logger != nil {
 		logger.Error(fmt.Sprintln(v...))
 	}
 }
 
-func Fatal(v ...interface{}) {
+func Fatal(v ...any) {
 	if logger != nil {
 		logger.Fatal(fmt.Sprintln(v...))
 	}
